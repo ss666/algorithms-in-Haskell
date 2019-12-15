03457a56e9d0669d69a217b8b484201ae89ba72d
s666 <390105636@qq.com>
2019-12-15 14:46:51 +0000

    .

---
+++ sorting.hs
@@ -0,0 +1,36 @@
+--Insertion Sort, run in O(n^2)
+insert :: Ord a => a -> [a] -> [a]
+insert x [] = [x]
+insert x (y:ys) = if x <= y then x:y:ys else insert x ys
+
+ins_sort :: Ord a => [a] -> [a]
+ins_sort [] =[]
+ins_sort (x:xs) = insert x (ins_sort xs)
+
+--other equivalent formulations
+ins_sort2 :: Ord a => [a] -> [a]
+ins_sort2 = foldr insert []
+
+ins_sort3 :: Ord a => [a] -> [a]
+ins_sort3 = foldl (flip insert) []
+ 
+
+--Quick Sort
+quick_sort :: Ord a => [a] -> [a]
+quick_sort (x:xs) = quick_sort smaller xs ++ [x] ++ quick_sort larger xs
+                where smaller = [a | a <- xs, a <= x]
+                      larger  = [b | b <- xs, b > x]
+
+
+--Merge Sort
+merge :: Ord a => [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge xs@(x:xs') ys@(y:ys') = if x<=y then x : merge xs' ys
+                                      else y : mrege xs ys'
+
+merge_sort :: Ord a => [a]
+merge_sort [] = []
+merge_sort xs = let (ys,zs) = splitAt (length xs `div` 2) xs
+                in merge (merge_sort ys) (merge_sort zs)
+
